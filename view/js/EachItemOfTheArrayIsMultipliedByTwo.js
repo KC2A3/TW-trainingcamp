14d6880b0b354dea57d0095fdc457504ed3fb0c3
KC2A3 <947496351@qq.com>
2018-11-22 23:44:33 +0800

    Multiply each item in the array by two

---
+++ view/js/EachItemOfTheArrayIsMultipliedByTwo.js
@@ -0,0 +1,5 @@
+var array = [1, 2, 3, 4, 5];
+var EachItemOfTheArrayIsMultipliedByTwo = a.map(function(item, index, array) {
+  return item * 2;
+});　　
+console.log(EachItemOfTheArrayIsMultipliedByTwo);
